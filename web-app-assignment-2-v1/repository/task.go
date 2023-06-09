7f636e65cfdecac82b42ad8ad02de756e53c7729
notalent2code <rihlanrihlan24@gmail.com>
2023-06-09 23:06:49 +0700

    init assignment 2 and change db creds

---
+++ web-app-assignment-2-v1/repository/task.go
@@ -0,0 +1,59 @@
+package repository
+
+import (
+	"a21hc3NpZ25tZW50/model"
+
+	"gorm.io/gorm"
+)
+
+type TaskRepository interface {
+	Store(task *model.Task) error
+	Update(task *model.Task) error
+	Delete(id int) error
+	GetByID(id int) (*model.Task, error)
+	GetList() ([]model.Task, error)
+	GetTaskCategory(id int) ([]model.TaskCategory, error)
+}
+
+type taskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepo(db *gorm.DB) *taskRepository {
+	return &taskRepository{db}
+}
+
+func (t *taskRepository) Store(task *model.Task) error {
+	err := t.db.Create(task).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *taskRepository) Update(task *model.Task) error {
+	return nil // TODO: replace this
+}
+
+func (t *taskRepository) Delete(id int) error {
+	return nil // TODO: replace this
+}
+
+func (t *taskRepository) GetByID(id int) (*model.Task, error) {
+	var task model.Task
+	err := t.db.First(&task, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &task, nil
+}
+
+func (t *taskRepository) GetList() ([]model.Task, error) {
+	return nil, nil // TODO: replace this
+}
+
+func (t *taskRepository) GetTaskCategory(id int) ([]model.TaskCategory, error) {
+	return nil, nil // TODO: replace this
+}
