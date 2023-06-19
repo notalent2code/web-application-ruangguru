d65d31e292c11036af5d45d2a43febde1787003f
notalent2code <rihlanrihlan24@gmail.com>
2023-06-19 10:45:03 +0700

    crud and check session repo

--- fcp-web-application-v1/repository/session.go
+++ fcp-web-application-v1/repository/session.go
@@ -24,24 +24,37 @@ func NewSessionsRepo(db *gorm.DB) *sessionsRepo {
 	return &sessionsRepo{db}
 }
 
-func (u *sessionsRepo) AddSessions(session model.Session) error {
-	return nil // TODO: replace this
+func (u *sessionsRepo) AddSessions(session model.Session) error {	// TODO: replace this
+	err := u.db.Create(&session)
+	return err.Error
 }
 
-func (u *sessionsRepo) DeleteSession(token string) error {
-	return nil // TODO: replace this
+func (u *sessionsRepo) DeleteSession(token string) error {			// TODO: replace this
+	err := u.db.Where("token = ?", token).Delete(&model.Session{})
+	return err.Error
 }
 
-func (u *sessionsRepo) UpdateSessions(session model.Session) error {
-	return nil // TODO: replace this
+func (u *sessionsRepo) UpdateSessions(session model.Session) error {// TODO: replace this
+	err := u.db.Where("email = ?", session.Email).Updates(session)
+	return err.Error
 }
 
-func (u *sessionsRepo) SessionAvailEmail(email string) (model.Session, error) {
-	return model.Session{}, nil // TODO: replace this
+func (u *sessionsRepo) SessionAvailEmail(email string) (model.Session, error) {// TODO: replace this
+	var result model.Session
+	err := u.db.Where("email = ?", email).First(&result).Error
+	if err != nil{
+		return model.Session{}, err
+	}
+	return result, nil
 }
 
-func (u *sessionsRepo) SessionAvailToken(token string) (model.Session, error) {
-	return model.Session{}, nil // TODO: replace this
+func (u *sessionsRepo) SessionAvailToken(token string) (model.Session, error) {// TODO: replace this
+	var result model.Session
+	err := u.db.Where("token = ?", token).First(&result).Error
+	if err != nil{
+		return model.Session{}, err 
+	}
+	return result, nil
 }
 
 func (u *sessionsRepo) TokenValidity(token string) (model.Session, error) {
